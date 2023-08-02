a704f92da250945c5f624a6fb96a8ecf5ac9ec70
GunardiCahyadi <cahyadigunardi@gmail.com>
2023-08-02 23:09:07 +0700

    env

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 const Controller = require("./controllers/controller");
 const cors = require("cors");
 // const public = require("./public");
